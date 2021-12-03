1edce342da0ba21f9886b7468201f0a9d769aa19
bennett-j <53816688+bennett-j@users.noreply.github.com>
2021-12-03 18:44:40 +0000

    Quick tidy up

--- src/arduino_main/arduino_main.ino
+++ src/arduino_main/arduino_main.ino
@@ -1,13 +1,15 @@
+// comment out to not compile debugging sections of code
 #define DEBUG
 
+// define Note as a new type
 typedef struct{
-    bool fPattern[10]; // array of bools for finger pattern (10 holes to cover)
+    bool fPattern[10]; // array of bools for finger pattern (10 holes to cover) 1 means hole covered
     int targetFreq; 
     int blowVel;
 } Note;
 
-//Note noteC5 = {{1,1,1,1,1,1,1,1,1,1}, 523, 0};
-
+// create a list of Note structures
+// C5 is MIDI note 85 so we will access the desired note using that offset
 // note range C5 to D6 is 15 notes
 Note notes[4] = {
   {{1,1,1,1,1,1,1,1,1,1}, 523, 0}, //C5
@@ -16,6 +18,7 @@ Note notes[4] = {
   {{1,1,1,1,1,1,1,0,0,0}, 622, 0}, //Eb5
 };
 
+// define pins the solenoids are connected to in hole order starting with thumb
 // usage: solenoidPins[2] returns pin corresponding to actuator covering hole 2
 int solenoidPins[10] = {2,3,4,5,6,7,8,9,10,11};
 
@@ -24,8 +27,11 @@ int solenoidPins[10] = {2,3,4,5,6,7,8,9,10,11};
 /////////////////////////
 
 void setFingers(bool pattern[]){
+    
     for (int f=0;f<10;f++){
+        // set pin high/low according to pattern
         digitalWrite(solenoidPins[f], pattern[f]);
+        
         #ifdef DEBUG
           Serial.print(solenoidPins[f]);
           Serial.print(": ");
@@ -34,20 +40,29 @@ void setFingers(bool pattern[]){
     }
 }
 
+/////////////////////////
+//        SETUP        //
+/////////////////////////
 
 void setup() {
+    
     Serial.begin(9600);
     
+    // set all solenoid pins for OUTPUT
     for (int f=0;f<10;f++){
         pinMode(solenoidPins[f], OUTPUT);
     }
 
+    // TESTING
     delay(2000);
-
     Serial.println("Set fingers note C5");
     Serial.print("Freq: ");
     Serial.println(notes[3].targetFreq);
     setFingers(notes[3].fPattern);
 }
+
+/////////////////////////
+//      MAIN LOOP      //
+/////////////////////////
 
 void loop(){}
