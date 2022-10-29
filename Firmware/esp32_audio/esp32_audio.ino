cede812a0fa55622839a69cab3fbffaa00228b1d
bennett-j <53816688+bennett-j@users.noreply.github.com>
2022-10-29 15:09:12 +0100

    audio reading

---
+++ Firmware/esp32_audio/esp32_audio.ino
@@ -0,0 +1,23 @@
+#include <I2S.h>
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  // A baud rate of 115200 is used instead of 9600 for a faster data rate
+  // on non-native USB ports
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // start I2S at 8 kHz with 32-bits per sample
+  if (!I2S.begin(ADC_DAC_MODE, 8000, 16)) {
+    Serial.println("Failed to initialize I2S!");
+    while (1); // do nothing
+  }
+}
+
+void loop() {
+  // read a sample
+  int sample = I2S.read();
+  Serial.println(sample);
+}
